96bff5998da26c709f1c462254fd85a658070c99
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-20 22:32:52 -0800

    updated and modify string

--- comments.php
+++ comments.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Initiator ( comments.php )
+ * Camaraderie ( comments.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @package     Camaraderie
+ * @copyright   Copyright (C) 2017-2020. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
@@ -19,17 +19,17 @@ if ( post_password_required() ) {
 <?php if ( comments_open() ) { ?>
 	<section id="comments-area" class="comments-area">
 		<?php if ( have_comments() ) { ?>
-			<h2 class="comments-title">
+			<h1 class="comments-title">
 				<?php $count = get_comments_number(); ?>
 				<?php
 				if ( '1' === $count ) {
-					printf( esc_html_x( 'One Comment', 'comments title', 'initiator' ) );
+					printf( esc_html_x( 'One Comment', 'comments title', 'camaraderie' ) );
 				} else {
 					// Translators: 1 = counts.
-					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'initiator' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
+					printf( _nx( '%1$s Comment', '%1$s Comments', absint( $count ), 'comments title', 'camaraderie' ), absint( number_format_i18n( $count ) ) ); // phpcs:ignore
 				}
 				?>
-			</h2>
+			</h1>
 		<?php } ?>
 		<ol class="comment-list">
 			<?php
@@ -37,15 +37,15 @@ if ( post_password_required() ) {
 					array(
 						'style'       => 'ol',
 						'short_ping'  => true,
-						'avatar_size' => 70,
+						'avatar_size' => 60,
 					)
 				);
 			?>
 		</ol>
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { ?>
 			<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'initiator' ) ); ?></div>
-				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'initiator' ) ); ?></div>
+				<div class="comment-previous"><?php previous_comments_link( '<i class="fa fa-arrow-circle-o-left"></i> ' . esc_html__( 'Older Comments', 'camaraderie' ) ); ?></div>
+				<div class="comment-next"><?php next_comments_link( '<i class="fa fa-arrow-circle-o-right"></i> ' . esc_html__( 'Newer Comments', 'camaraderie' ) ); ?></div>
 			</nav>
 		<?php } ?>
 		<?php comment_form(); ?>
