bd5d7214955fbf985333ac47a40be3202b7548cd
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-20 23:44:41 -0800

    post-thumbnail

--- public/views/content/content.php
+++ public/views/content/content.php
@@ -1,17 +1,23 @@
 <?php
 /**
- * Initiator ( content.php )
+ * Camaraderie ( content.php )
  *
- * @package     Initiator
+ * @package     Camaraderie
  * @copyright   Copyright (C) 2018-2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<picture class="post-thumbnail">
+		<?php if ( has_post_thumbnail() ) { ?>
+			<source media="(min-width: 601px) and (max-width: 768px)" srcset="<?php the_post_thumbnail_url( 'camaraderie-medium-thumbnails' ); ?>">
+			<?php the_post_thumbnail( 'camaraderie-small-thumbnails' ); ?>
+		<?php } ?>
+	</picture>
 	<header class="entry-header">
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
-			<?php printf( '<span class="sticky-post">%1$s</span>', esc_html__( 'Featured', 'initiator' ) ); ?>
+			<?php printf( '<span class="sticky-post">%1$s</span>', esc_html__( 'Featured', 'camaraderie' ) ); ?>
 		<?php } ?>
 		<?php Benlumia007\Backdrop\Entry\display_title(); ?>
 		<div class="entry-metadata">
