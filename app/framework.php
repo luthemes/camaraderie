6d05a6282a3e7c8788cfc181e3aeb8ca180a28d4
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-20 21:27:48 -0800

    change strings to camaraderie and add Customize feature

--- app/framework.php
+++ app/framework.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Initiator ( framework.php )
+ * Camaraderie ( framework.php )
  *
  * This file is used to create a new framework instance and adds specific features to the theme.
  *
- * @package     Initiator
+ * @package     Camaraderie
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
@@ -15,19 +15,21 @@
  *
  * This will create an instance of the framework allowing you to initialize the theme.
  */
-$initiator = Benlumia007\Backdrop\Framework::get_instance();
+$camaraderie = Benlumia007\Backdrop\Framework::get_instance();
 
-$initiator->menus = new Benlumia007\Backdrop\Menu\Menu(
+$camaraderie->menus = new Benlumia007\Backdrop\Menu\Menu(
 	$args = [
-		'primary' => esc_html__( 'Primary Navigation', 'initiator' ),
+		'primary' => esc_html__( 'Primary Navigation', 'Camaraderie' ),
 	]
 );
 
-$initiator->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
+$camaraderie->sidebars = new Benlumia007\Backdrop\Sidebar\Sidebar(
 	$args = [
 		'primary' => [
-			'name' => esc_html__( 'Primary Sidebar', 'initiator' ),
-			'desc' => esc_html__( 'Test', 'initiator' ),
+			'name' => esc_html__( 'Primary Sidebar', 'Camaraderie' ),
+			'desc' => esc_html__( 'Test', 'Camaraderie' ),
 		],
 	]
 );
+
+$camaraderie->customize = new Camaraderie\Component\Customize();
