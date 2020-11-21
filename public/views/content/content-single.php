3faa424f6c8a568a44048983017bc0f1d5d1e3f7
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-20 21:23:52 -0800

    readjust the metadata since it's supposed to be inside the header tag

--- public/views/content/content-single.php
+++ public/views/content/content-single.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Initiator ( content-single.php )
+ * Camaraderie ( content-single.php )
  *
- * @package     Initiator
- * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @package     Camaraderie
+ * @copyright   Copyright (C) 2017-2020. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
@@ -11,22 +11,22 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php Benlumia007\Backdrop\Entry\display_title(); ?>
+		<div class="entry-metadata">
+			<?php Benlumia007\Backdrop\Entry\display_author(); ?>
+			<?php Benlumia007\Backdrop\Entry\display_date(); ?>
+			<?php Benlumia007\Backdrop\Entry\display_comments_link(); ?>
+		</div>
 	</header>
-	<div class="entry-metadata">
-		<?php Benlumia007\Backdrop\Entry\display_author(); ?>
-		<?php Benlumia007\Backdrop\Entry\display_date(); ?>
-		<?php Benlumia007\Backdrop\Entry\display_comments_link(); ?>
-	</div>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages(
 				array(
-					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'initiator' ),
+					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'camaraderie' ),
 					'after'       => '</div>',
 					'link_before' => '<span>',
 					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'initiator' ) . ' </span>%',
+					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'camaraderie' ) . ' </span>%',
 					'separator'   => '<span class="screen-reader-text">,</span> ',
 				)
 			);
