7bf9a5cdb30c460ca27a4f661e94366066ca90f4
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-22 13:18:58 -0800

    add portfolio section

---
+++ public/views/section/content-portfolio.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Camaraderie ( content-single.php )
+ *
+ * @package     Camaraderie
+ * @copyright   Copyright (C) 2017-2020. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+?>
+<section id="portflio" class="site-portfolio">
+    <header id="header" class="portfolio-header">
+        <h1 class="portfolio-title"><?php esc_html_e( 'Portfolio', 'camaraderie' ); ?></h1>
+    </header>
+</section>
