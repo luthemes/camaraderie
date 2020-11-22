5fa2f1dbcb630de6d58ed23bc38efc895ac55820
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-22 13:16:34 -0800

    add portfolio, blog, and contact section

--- templates/template-home.php
+++ templates/template-home.php
@@ -9,7 +9,18 @@
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
+$portfolio_display = get_theme_mod( 'custom_portfolio_display' );
+$blog_display      = get_theme_mod( 'custom_blog_display' );
+$contact_display   = get_theme_mod( 'custom_contact_display' );
 ?>
 <?php get_header(); ?>
-
+    <?php if ( 0 != $portfolio_display && isset( $portfolio_display ) ) { // phpcs:ignore ?>
+        <?php Benlumia007\Backdrop\Template\get_template_part( 'section/content', 'portfolio' ); ?>
+    <?php } ?>
+    <?php if ( 0 != $blog_display && isset( $blog_display ) ) { // phpcs:ignore ?>
+        <?php Benlumia007\Backdrop\Template\get_template_part( 'section/content', get_post_format() ); ?>
+    <?php } ?>
+    <?php if ( 0 != $contact_display && isset( $contact_display ) ) { // phpcs:ignore ?>
+        <?php Benlumia007\Backdrop\Template\get_template_part( 'section/content', get_post_format() ); ?>
+    <?php } ?>
 <?php get_footer(); ?>
