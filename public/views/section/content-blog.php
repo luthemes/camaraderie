fe1c0888844046832a48bd12620d3bb803a07b2c
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-22 16:41:07 -0800

    content-blog

---
+++ public/views/section/content-blog.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Camaraderie ( content-single.php )
+ *
+ * @package     Camaraderie
+ * @copyright   Copyright (C) 2017-2020. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+?>
+<section id="blog" class="site-blog">
+    <div class="blog-container">
+        <header id="header" class="blog-header">
+            <h1 class="blog-title"><?php esc_html_e( 'Blog', 'camaraderie' ); ?></h1>
+            <span class="blog-description"><?php esc_html_e( 'Work That I have done', 'camaraderie' ); ?></span>
+        </header>
+    </div>
+</section>
