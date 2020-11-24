feb00b9c9676840f1e4a2247f6d97f65aa834178
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-23 21:37:39 -0800

    worked on blog content in the sections

--- public/views/section/content-blog.php
+++ public/views/section/content-blog.php
@@ -9,10 +9,46 @@
  */
 ?>
 <section id="blog" class="site-blog">
-    <div class="blog-container">
+    <div class="content-area">
         <header id="header" class="blog-header">
             <h1 class="blog-title"><?php esc_html_e( 'Blog', 'camaraderie' ); ?></h1>
-            <span class="blog-description"><?php esc_html_e( 'Work That I have done', 'camaraderie' ); ?></span>
+            <span class="blog-description"><?php esc_html_e( 'Latest News', 'camaraderie' ); ?></span>
         </header>
+        <div class="entry-content">
+            <ul class="blog-grid">
+                <?php
+                    $posts_per_page = 3;
+                    $query          = new \WP_Query(
+                        array(
+                            'post_type'           => 'post',
+                            'ignore_sticky_posts' => 1,
+                            'posts_per_page'      => $posts_per_page,
+                        )
+                    );
+
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                            $query->the_post();
+                            ?>
+                                <li>
+                                    <?php the_post_thumbnail( 'camaraderie-medium-thumbnails' ); ?>
+                                    <header class="entry-header">
+                                        <h1 class="entry-title"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h1>
+                                        <div class="entry-metadata">
+                                            <?php Benlumia007\Backdrop\Entry\display_author(); ?>
+                                            <?php Benlumia007\Backdrop\Entry\display_date(); ?>
+                                            <?php Benlumia007\Backdrop\Entry\display_comments_link(); ?>
+                                        </div>
+                                    </header>
+                                    <div class="entry-excerpt">
+                                        <?php the_excerpt(); ?>
+                                    </div>
+                                </li>
+                            <?php
+                        }
+                    }
+                ?>
+            </ul>
+        </div>
     </div>
 </section>
