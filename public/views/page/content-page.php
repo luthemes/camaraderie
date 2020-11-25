34b384f2fb1877cf5fc5eb8f164100fd2030d006
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-24 20:02:45 -0800

    initial commit

---
+++ public/views/page/content-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Camaraderie ( content-page.php )
+ *
+ * @package     Camaraderie
+ * @copyright   Copyright (C) 2017-2020. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php Benlumia007\Backdrop\Entry\display_title(); ?>
+	</header>
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages(
+				array(
+					'before'      => '<div class="page-links">' . esc_html__( 'Pages:', 'camaraderie' ),
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+					'pagelink'    => '<span class="screen-reader-text">' . esc_html__( 'Page', 'camaraderie' ) . ' </span>%',
+					'separator'   => '<span class="screen-reader-text">,</span> ',
+				)
+			);
+			?>
+	</div>
+</article>
