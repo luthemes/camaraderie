f1e51a8f81057ff70182186fca705d06a6a07231
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-11-25 20:43:46 -0800

    change entry title

--- public/views/archive/content-portfolio.php
+++ public/views/archive/content-portfolio.php
@@ -10,7 +10,7 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-        <?php Benlumia007\Backdrop\Entry\display_title(); ?>
+        <h1 class="entry-title"><?php esc_html_e( 'Portfolio', 'camaraderie' ); ?></h1>
 	</header>
     <div class="entry-content">
         <ul class="portfolio-grid">
