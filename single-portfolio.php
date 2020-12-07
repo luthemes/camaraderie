183ffab5560ae3b9f531365eb6ec6e6e8fc94f98
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-12-06 20:31:35 -0800

    add related items below

--- single-portfolio.php
+++ single-portfolio.php
@@ -22,4 +22,36 @@
 			<?php Benlumia007\Backdrop\View\display( 'sidebar', [ 'portfolio' ] ); ?>
 		</div>
 	</section>
+	<div id="related-items" class="related-items">
+		<div class="entry-content">
+			<ul class="portfolio-grid">
+				<?php $query = new WP_Query( array(
+					'post_type'         => 'portfolio', 
+					'posts_per_page'    => 3, 
+					'orderby'           => 'rand',
+					'post__not_in'      => array( get_queried_object_id() )
+				) ); 
+				?>
+				<?php if ($query->have_posts()) { ?>
+					<?php while ($query->have_posts()) { ?>
+						<?php $query->the_post(); ?>
+							<?php if ( has_post_thumbnail() ) { ?>
+							<li>
+								<a href="<?php echo esc_url(get_permalink()); ?>">
+									<?php the_post_thumbnail('camaraderie-large-thumbnails'); ?>
+								</a>
+							<div class="wp-caption">
+								<div class="wp-caption-text">
+									<h3 class="portfolio-title"><a href="<?php echo esc_url(get_permalink()); ?>"><?php the_title(); ?></a></h3>
+									<span><?php echo wptexturize( wp_strip_all_tags( get_post( get_post_thumbnail_id() )->post_content ) ); // phpcs:ignore ?></span>
+								</div>
+							</div>
+							</li>
+						<?php } ?>
+					<?php } ?> 
+					<?php wp_reset_postdata(); ?>
+				<?php } ?>
+			</ul>
+		</div>
+	</div>
 <?php get_footer(); ?>
