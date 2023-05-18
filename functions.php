9124b944537febb72fafce9be31863ffa06be8c1
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-05-18 10:55:58 -0700

    let's continue to make some changes to this theme

--- functions.php
+++ functions.php
@@ -1,68 +1,23 @@
 <?php
 /**
- * Camaraderie ( functions.php )
+ * Default functions template
  *
- * This (functions.php) template file should only do two jobs, one is to check the compatibility if the site meets
- * the miminum requirements before the theme proceeds to activate. The second job is to autoload the backdrop core
- * famework.
+ * This file is used to bootstrap the theme.
  *
- * @package     Camaraderie
- * @copyright   Copyright (C) 2017-2020. Benjamin Lu
- * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
- * @author      Benjamin Lu ( https://benjlu.com )
+ * @package   Camaraderie
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2017-2023. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/camaraderie
  */
 
-/**
- * Table of Content
- *
- * 1.0 - Compatibility Check
- * 2.0 - Backdrop Core
- */
+# ------------------------------------------------------------------------------
+# Load the composer files
+# ------------------------------------------------------------------------------
+#
+# Load the composer files. Note that autoloading should happen first so that
+# any classes or functions are available that we might need.
 
-/**
- * 1.0 - Compatibility Check
- */
-function camaraderie_compatibility_check() {
-	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) ) {
-		return sprintf(
-			// translators: 1 =  a version string, 2 = current wp version string.
-			__( 'camaraderie requires at least WordPress version %1$s. You are currently running %2$s. Please upgrade and try again.', 'camaraderie' ),
-			'4.9',
-			$GLOBALS['wp_version']
-		);
-	} elseif ( version_compare( PHP_VERSION, '7.0', '<' ) ) {
-		return sprintf(
-			// translators: 1 =  a version string, 2 = current wp version string.
-			__( 'camaraderie requires at least PHP version %1$s. You are currently running %2$s. Please upgrade and try again.', 'camaraderie' ),
-			'7.0',
-			PHP_VERSION
-		);
-	}
-	return '';
-}
-
-/**
- * Triggered after switch themes and check if it meets the requirements.
- */
-function camaraderie_switch_theme() {
-	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) || version_compare( PHP_VERSION, '7.0', '<' ) ) {
-		switch_theme( get_option( 'theme_switched' ) );
-		add_action( 'admin_notices', 'camaraderie_upgrade_notice' );
-	}
-	return false;
-}
-add_action( 'after_switch_theme', 'camaraderie_switch_theme' );
-
-/**
- * Displays an error if it doesn't meet the requirements.
- */
-function camaraderie_upgrade_notice() {
-	printf( '<div class="error"><p>%s</p></div>', esc_html( camaraderie_compatibility_check() ) );
-}
-
-/**
- * 2.0 - Backdrop Core
- */
 if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
 	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
 }
